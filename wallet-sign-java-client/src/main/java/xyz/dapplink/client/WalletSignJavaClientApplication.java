b83d18531afc94f3ba407097226ae58ea9d61f94
RayL <rayliu8499@qq.com>
2024-10-05 12:28:24 +0800

    init

---
+++ wallet-sign-java-client/src/main/java/xyz/dapplink/client/WalletSignJavaClientApplication.java
@@ -0,0 +1,13 @@
+package xyz.dapplink.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletSignJavaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletSignJavaClientApplication.class, args);
+    }
+
+}
---
+++ wallet-sign-java-interface/src/main/java/xyz/dapplink/iface/WalletSignJavaInterfaceApplication.java
@@ -0,0 +1,13 @@
+package xyz.dapplink.iface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletSignJavaInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletSignJavaInterfaceApplication.class, args);
+    }
+
+}
