7bacf40dbc815c8464f3d2dce12408526f5cf43b
RayL <rayliu8499@qq.com>
2024-10-06 09:27:59 +0800

    fix(strategy): 1.fix secp256k1

--- wallet-sign-java-server/src/main/java/xyz/dapplink/server/algorithm/EcDSAStrategy.java
+++ wallet-sign-java-server/src/main/java/xyz/dapplink/server/algorithm/EcDSAStrategy.java
@@ -1,10 +1,9 @@
 package xyz.dapplink.server.algorithm;
 
-
-import org.bouncycastle.jce.ECNamedCurveTable;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
 
+import org.bouncycastle.jce.spec.ECNamedCurveSpec;
 import org.springframework.stereotype.Component;
 import xyz.dapplink.server.algorithm.dto.PairEntity;
 import xyz.dapplink.server.enums.SignType;
@@ -12,6 +11,7 @@ import xyz.dapplink.server.enums.SignType;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.Security;
+import java.security.spec.ECParameterSpec;
 import java.util.Base64;
 
 @Component
@@ -29,8 +29,10 @@ public class EcDSAStrategy implements AlgorithmStrategy {
     public PairEntity generateKeygen() throws Exception {
         Security.addProvider(new BouncyCastleProvider());
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("EC", "BC");
-        ECNamedCurveParameterSpec secp256k1 = ECNamedCurveTable.getParameterSpec("secp256k1");
-        keyGen.initialize(secp256k1);
+        ECNamedCurveParameterSpec secp256k1 = (ECNamedCurveParameterSpec) org.bouncycastle.jce.provider.BouncyCastleProvider.CONFIGURATION.getEcImplicitlyCa();
+        ECParameterSpec ecSpec = new ECNamedCurveSpec("secp256k1", secp256k1.getCurve(), secp256k1.getG(), secp256k1.getN());
+
+        keyGen.initialize(ecSpec);
         KeyPair keyPair = keyGen.generateKeyPair();
         return new PairEntity()
                 .setPublicKey(Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded()))
