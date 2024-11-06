a681c3d35788f40a7c24236b63feafd0ae6fd9b9
RayL <rayliu8499@qq.com>
2024-11-06 16:58:59 +0800

    feat: 1.update

--- wallet-sign-java-server/src/main/java/xyz/dapplink/server/algorithm/EcDSAStrategy.java
+++ wallet-sign-java-server/src/main/java/xyz/dapplink/server/algorithm/EcDSAStrategy.java
@@ -1,5 +1,6 @@
 package xyz.dapplink.server.algorithm;
 
+import jakarta.annotation.PostConstruct;
 import org.bouncycastle.asn1.sec.SECNamedCurves;
 import org.bouncycastle.asn1.sec.SECObjectIdentifiers;
 import org.bouncycastle.asn1.x9.X9ECParameters;
@@ -25,12 +26,25 @@ import java.util.Base64;
 @Component
 public class EcDSAStrategy implements AlgorithmStrategy {
 
+    private ECKeyPairGenerator generator;
+
     static {
         Security.addProvider(new BouncyCastleProvider());
     }
 
     private final String type = SignType.ECDSA.getName();
 
+    @PostConstruct
+    public void init() {
+        // 选择椭圆曲线参数，这里以 secp256k1 为例
+        X9ECParameters ecParams = SECNamedCurves.getByOID(SECObjectIdentifiers.secp256k1);
+        ECDomainParameters domainParams = new ECDomainParameters(ecParams.getCurve(), ecParams.getG(), ecParams.getN(), ecParams.getH());
+
+        // 创建密钥对生成器并初始化
+        generator = new ECKeyPairGenerator();
+        ECKeyGenerationParameters genParams = new ECKeyGenerationParameters(domainParams, new SecureRandom());
+        generator.init(genParams);
+    }
 
     @Override
     public String getTypeName() {
@@ -39,14 +53,6 @@ public class EcDSAStrategy implements AlgorithmStrategy {
 
     @Override
     public KeyPairDto generateKeygen() throws Exception {
-        // 选择椭圆曲线参数，这里以 secp256k1 为例
-        X9ECParameters ecParams = SECNamedCurves.getByOID(SECObjectIdentifiers.secp256k1);
-        ECDomainParameters domainParams = new ECDomainParameters(ecParams.getCurve(), ecParams.getG(), ecParams.getN(), ecParams.getH());
-
-        // 创建密钥对生成器并初始化
-        ECKeyPairGenerator generator = new ECKeyPairGenerator();
-        ECKeyGenerationParameters genParams = new ECKeyGenerationParameters(domainParams, new SecureRandom());
-        generator.init(genParams);
 
         // 生成密钥对
         AsymmetricCipherKeyPair keyPair = generator.generateKeyPair();
