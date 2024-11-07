b640668e65c0f1348c8b1af0e2918ccc6d359f07
RayL <rayliu8499@qq.com>
2024-11-07 13:10:45 +0800

    feat: 1.to init list and map with capacity

--- wallet-sign-java-server/src/main/java/xyz/dapplink/server/service/impl/AccountServiceImpl.java
+++ wallet-sign-java-server/src/main/java/xyz/dapplink/server/service/impl/AccountServiceImpl.java
@@ -5,12 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import xyz.dapplink.iface.lib.PublicKey;
+import xyz.dapplink.server.algorithm.AlgorithmStrategy;
 import xyz.dapplink.server.algorithm.dto.KeyPairDto;
 import xyz.dapplink.server.enums.SignType;
 import xyz.dapplink.server.service.AlgorithmService;
 import xyz.dapplink.server.service.IAccountService;
 import xyz.dapplink.server.service.LevelDBService;
+import xyz.dapplink.server.utils.HexStringUtils;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 @Slf4j
@@ -25,19 +28,20 @@ public class AccountServiceImpl implements IAccountService {
     @Override
     public List<PublicKey> generateKeyGen(int number, SignType signType) {
         Assert.isTrue(number > 0 && number <= 100000, "invalid numbers");
-        List<PublicKey> keyList = new LinkedList<>();
-        Map<byte[], byte[]> objList = new HashMap<>();
+        List<PublicKey> keyList = new ArrayList<>(number);
+        Map<byte[], byte[]> objList = new HashMap<>(number);
+        AlgorithmStrategy strategyService = algorithmService.getStrategy(signType);
+        log.info("start generate: {}", LocalDateTime.now());
         for (int i = 0; i < number; i++) {
             KeyPairDto keyPairDto;
             try {
-                keyPairDto = algorithmService.getStrategy(signType).generateKeygen();
+                keyPairDto = strategyService.generateKeygen();
                 objList.put(keyPairDto.getPublicKey().getBytes(), keyPairDto.getPrivateKey().getBytes());
             } catch (Exception e) {
                 log.error("generateKeyGen error:{}", e.getMessage(), e);
                 throw new RuntimeException(e.getMessage());
             }
-            PublicKey key = PublicKey.newBuilder().setCompressPubkey(keyPairDto.getCompressPublicKey()).setDecompressPubkey(keyPairDto.getPublicKey()).build();
-            keyList.add(key);
+            keyList.add(PublicKey.newBuilder().setCompressPubkey(keyPairDto.getCompressPublicKey()).setDecompressPubkey(keyPairDto.getPublicKey()).build());
         }
         dbService.batchSave(objList);
         return keyList;
@@ -45,7 +49,9 @@ public class AccountServiceImpl implements IAccountService {
 
     @Override
     public String sign(String publicKey, String msg) {
-
+        Assert.isTrue(msg.length() == 32, "invalid string length");
+        byte[] bytesPublicKey = dbService.getByPublicKey(HexStringUtils.hexStringToByteArray(publicKey));
+        Assert.notNull(bytesPublicKey, "empty result");
         return null;
     }
 }
